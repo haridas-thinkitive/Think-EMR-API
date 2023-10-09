2b4e760d5a6acd372d8295b62c97e5bca8c2aa22
Haridas-Dhulgande <dhulgandeharidas83@gmail.com>
2023-10-09 13:26:28 +0530

    Added-DB-Setup

---
+++ ThinkEMR.Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkEMR_Care.DataAccess.Models;
+
+namespace ThinkEMR_Care.Core.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Collaborator> collaborators { get; set; }
+    }
+}
