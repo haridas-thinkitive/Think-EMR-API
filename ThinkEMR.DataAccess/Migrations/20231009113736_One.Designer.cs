51f2a7f724f2bd3bf5f6e086357afed7e89b59cd
Haridas-Dhulgande <dhulgandeharidas83@gmail.com>
2023-10-10 12:21:28 +0530

    Add and Changes in Template structure

--- ThinkEMR.DataAccess/Migrations/20231009113736_One.Designer.cs
+++ ThinkEMR.DataAccess/Migrations/20231009113736_One.Designer.cs
@@ -2,17 +2,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ThinkEMR_Care.Core.Data;
+using ThinkEMR_Care.DataAccess.Data;
 
 #nullable disable
 
-namespace ThinkEMR_Care.Core.Migrations
+namespace ThinkEMR_Care.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231009113736_One")]
+    partial class One
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ThinkEMR.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ ThinkEMR.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,20 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ThinkEMR_Care.Core.Data;
+using ThinkEMR_Care.DataAccess.Data;
 
 #nullable disable
 
-namespace ThinkEMR_Care.Core.Migrations
+namespace ThinkEMR_Care.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20231009075435_One")]
-    partial class One
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
