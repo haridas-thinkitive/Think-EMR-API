63046acf80a03f40d4dbe6512db16e89436f83e7
Haridas-Dhulgande <dhulgandeharidas83@gmail.com>
2023-11-03 12:42:53 +0530

    1. Add ProviderGroup services and repo service in middleware

---
+++ ThinkEMR.DataAccess/Models/AdminProfile/AdminInformation.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkEMR_Care.DataAccess.Models.AdminProfile
+{
+    public class AdminInformation
+    {
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("contact_number")]
+        public string ContactNumber { get; set; }
+
+        [JsonProperty("role")]
+        public string Role { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("modified_at")]
+        public DateTime ModifiedAt { get; set; }
+
+        [JsonProperty("created_by")]
+        public string CreatedBy { get; set; }
+
+        [JsonProperty("modified_by")]
+        public string ModifiedBy { get; set; }
+
+    }
+
+}
